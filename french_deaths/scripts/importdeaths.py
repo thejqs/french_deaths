28d9f826450b7b5e7a47abe348609b6fe20af731
Jacob Sanders <j.q.sanders@gmail.com>
2015-10-09 12:23:56 -0400

    cleaning up the All Causes row

--- french_deaths/scripts/importdeaths.py
+++ french_deaths/scripts/importdeaths.py
@@ -6,7 +6,6 @@ import sys
 import re
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "french_deaths.settings")
-
 sys.path.append("..")
 
 from django.db.models import Q
@@ -25,12 +24,13 @@ with open(french_deaths_csv, 'r') as csv_file:
     MorirCause.objects.all().delete()
 
     for row in reader:
-        cause = re.sub(r'\(.*?\)', '', row["ICD10"])
+        cause = re.sub(r'\(.*?\)', '', row['ICD10'])
+        cause.replace(' excluding S00-T98', '')
         new_cause, created = MorirCause.objects.get_or_create(cause=cause)
         new_cause.save()
 
         new_death = Morir.objects.create(
-            number_of_deaths=row["Value"].replace(':', '0').replace(' ', ''),
+            number_of_deaths=row['Value'].replace(':', '0').replace(' ', ''),
             year=row["TIME"],
             sex=row["SEX"]
         )
